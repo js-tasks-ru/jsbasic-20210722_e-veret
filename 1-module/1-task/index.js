89533be7f09dde9cfa7d930600c731bd4bebdef7
elveret <lena.veret@gmail.com>
2021-07-23 16:33:19 +0300

    feat: implement 1-module-1-task

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,11 @@
 function factorial(n) {
-  // ваш код...
+  if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    let result = n;
+    for (i = 1; i < n; i++) {
+      result = result * (n - i);
+    }
+    return result;
+  }
 }
