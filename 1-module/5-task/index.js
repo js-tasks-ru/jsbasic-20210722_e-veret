fdaab67540e22cea623706074ed7f79d69b423ab
elveret <lena.veret@gmail.com>
2021-07-27 22:10:08 +0300

    fix: update 1-task more

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,3 @@
 function truncate(str, maxlength) {
-  return str.length > maxlength ? str.slice(0, maxlength) + "\u2026" : str;
+  return str.length > maxlength ? str.slice(0, maxlength - 1) + "\u2026" : str;
 }
