4946a85581b0ccd78e0f2482a6a75664a22b6db8
elveret <lena.veret@gmail.com>
2021-08-25 09:05:03 +0300

    feat: implement 5-module/3-task

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,44 @@
 function initCarousel() {
-  // ваш код...
+  let carousel = document.querySelector(".carousel");
+  carousel.querySelector(".carousel__arrow_left").style.display = "none";
+
+  let inner = carousel.querySelector(".carousel__inner");
+
+  let delta = inner.offsetWidth;
+  let counter = 1;
+  let maxCounter = Array.from(
+    inner.querySelectorAll(".carousel__slide")
+  ).length;
+
+  carousel.addEventListener("click", function (event) {
+    if (!event.target.classList.contains("carousel__arrow")) {
+      return;
+    }
+
+    if (event.target.classList.contains("carousel__arrow_right")) {
+      inner.style.transform = `translateX(-${delta * counter}px)`;
+
+      counter += 1;
+
+      if (counter === maxCounter) {
+        event.target.style.display = "none";
+      }
+      if (counter === 2) {
+        carousel.querySelector(".carousel__arrow_left").style.display = "";
+      }
+    }
+
+    if (event.target.classList.contains("carousel__arrow_left")) {
+      inner.style.transform = `translateX(-${delta * (counter - 2)}px)`;
+
+      counter -= 1;
+
+      if (counter === 1) {
+        event.target.style.display = "none";
+      }
+      if (counter === 3) {
+        carousel.querySelector(".carousel__arrow_right").style.display = "";
+      }
+    }
+  });
 }
