30a9a585f51f7cd473ebefbfe0833a29b09f4a6d
elveret <lena.veret@gmail.com>
2021-08-27 19:34:13 +0300

    feat: implement 6-module/2-task

--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,4 +1,37 @@
+import createElement from "../../assets/lib/create-element.js";
+
 export default class ProductCard {
   constructor(product) {
+    this.product = product;
+    this.render();
+
+    let button = this.elem.querySelector(".card__button");
+    button.addEventListener("click", this.addProduct);
+  }
+
+  addProduct = () => {
+    let custormEvent = new CustomEvent("product-add", {
+      detail: this.product.id,
+      bubbles: true,
+    });
+
+    this.elem.dispatchEvent(custormEvent);
+  };
+
+  render() {
+    this.elem = createElement(`<div class="card">
+    <div class="card__top">
+    <img src="/assets/images/products/${
+      this.product.image
+    }" class="card__image" alt="product">
+    <span class="card__price">&euro;${this.product.price.toFixed(2)}</span>
+    </div>
+    <div class="card__body">
+    <div class="card__title">${this.product.name}</div>
+    <button type="button" class="card__button">
+    <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+    </button>
+    </div>
+</div>`);
   }
 }
