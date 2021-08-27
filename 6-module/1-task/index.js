fb424bf7dd65db8fb9c7b165b651f229b42d97bb
elveret <lena.veret@gmail.com>
2021-08-27 15:36:06 +0300

    feat: implement 6-module/1-task

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,38 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.elem = document.createElement("table");
+    this.render(rows);
+
+    this.elem.addEventListener("click", this.onclick);
   }
+
+  render(rows) {
+    let thead = `<thead><tr>`;
+    let tbody = `<tbody>`;
+    rows.forEach((element, index) => {
+      tbody = tbody + `<tr>`;
+      for (let key in element) {
+        if (index == 0) {
+          thead = thead + `<th>${key}</th>`;
+        }
+
+        tbody = tbody + `<td>${element[key]}</td>`;
+      }
+
+      tbody = tbody + `<td><button>X</button></td></tr>`;
+    });
+    thead = thead + `<th></th></tr></thead>`;
+
+    tbody = tbody + `</tbody>`;
+    this.elem.innerHTML = thead + tbody;
+  }
+
+  onclick = () => {
+    if (!event.tagName === "BUTTON") {
+      return;
+    }
+    let current = event.target.closest("tr");
+    current.remove();
+  };
 }
